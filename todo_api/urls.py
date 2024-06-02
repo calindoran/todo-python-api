1b2f4f015770aeff8f36fe57556f7dbccd12b310
Calin <calindoran@gmail.com>
2024-06-02 19:18:14 +0100

    add get users to api

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
 from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -35,9 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include('todo_app.urls')),
-    path('api-auth/', include('rest_framework.urls')),
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import HelloWorldView
+from .views import GetUsersView
 
 urlpatterns = [
     path('hello/', HelloWorldView.as_view(), name='hello-world'),
+    path('getUsers/', GetUsersView.as_view(), name='get-users'),
 ]
